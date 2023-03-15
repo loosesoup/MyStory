b6fca545e6cebb4832b9202b505fdf222033bc90
loosesoup <avonabrahamsjr@live.com>
2023-03-15 17:22:32 -0400

    base reqs complete

--- MyStory/ViewController.swift
+++ MyStory/ViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let whereButtonInfo = Information(paragraph: "Gin Rummy was born and raised in Woodcrest, MD", image: UIImage(named: "Minneapolis"))
-    let occupationButtonInfo = Information(paragraph: "He's an Iraq war veteran, but now he picks up small jobs from Wuncler Industries", image: UIImage(named: "Walmart"))
-    let HobbiesButtonInfo = Information(paragraph: "clumsy bank robber", image: UIImage(named: "The_Goat2"))
+    let whereButtonInfo = Information(paragraph: "Gin Rummy was born and raised in Woodcrest, MD", image: UIImage(named: "woodcrest boondocks"))
+    let occupationButtonInfo = Information(paragraph: "He's an Iraq war veteran, but now he picks up small jobs from Wuncler Industries", image: UIImage(named: "ginrummy war"))
+    let HobbiesButtonInfo = Information(paragraph: "Likes robbing people / places. Professional menace to society ", image: UIImage(named: "gin rummy savage"))
 
     override func viewDidLoad() {
         super.viewDidLoad()
